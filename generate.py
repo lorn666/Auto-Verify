8855d352095d6570f0955e5c75c47b8a89efd21a
lorntz <lorntz273@gmail.com>
2025-01-19 16:28:20 -0800

    no summary

--- generate.py
+++ generate.py
@@ -9,8 +9,8 @@ import jsonlines
 from tqdm import tqdm
 from itertools import islice
 
-device='cuda:0'
-verifier_device = 'cuda:0'
+device='cuda:1'
+verifier_device = 'cuda:1'
 max_new_tokens = 512
 verifier_max_new_tokens = 256
 model_path = "meta-llama/Llama-3.1-8B-Instruct"
