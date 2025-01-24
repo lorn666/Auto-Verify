bd63328dad499c697381f25a00673d7f971c76bd
lorntz <lorntz273@gmail.com>
2025-01-24 09:06:37 -0800

    add no yes to gemma

--- final_code_cxr/ov+ns/gemma-2-9b-it.py
+++ final_code_cxr/ov+ns/gemma-2-9b-it.py
@@ -10,7 +10,7 @@ from tqdm import tqdm
 from itertools import islice
 
 device = "cuda:0"
-verifier_device = "cuda:7"
+verifier_device = "cuda:0"
 max_new_tokens = 512
 verifier_max_new_tokens = 256
 # model_path = "deepseek-ai/deepseek-llm-7b-base"
--- final_code_cxr/ov+s32/gemma-2-9b-it.py
+++ final_code_cxr/ov+s32/gemma-2-9b-it.py
@@ -9,8 +9,8 @@ import jsonlines
 from tqdm import tqdm
 from itertools import islice
 
-device = "cuda:0"
-verifier_device = "cuda:7"
+device = "cuda:1"
+verifier_device = "cuda:1"
 max_new_tokens = 512
 verifier_max_new_tokens = 256
 model_path = "google/gemma-2-9b-it"
